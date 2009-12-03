c18b7f90ad416d1740aab8be82da5e9029947db2
David Dorward <david@dorward.me.uk>
2009-12-03 23:28:18 +0000

    Initial skeleton

---
+++ lib/Catalyst/TraitFor/View/MarkupValidation.pm
@@ -0,0 +1,26 @@
+package Catalyst::TraitFor::View::MarkupValidation;
+use Moose;
+use namespace::autoclean;
+
+1;
+
+=head1 NAME
+
+Catalyst::TraitFor::View::MarkupValidation - 
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 BUGS
+
+=head1 AUTHOR
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 the above author(s).
+
+This sofware is free software, and is licensed under the same terms as perl itself.
+
+=cut
+
